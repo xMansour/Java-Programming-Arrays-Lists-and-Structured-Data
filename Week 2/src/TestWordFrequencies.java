6d117bef97e5095752283649486332f473b32331
xMansour <melsayedmansour96@gmail.com>
2019-02-18 20:00:40 +0200

    Week 2. Assignment 1. Part1. WordFrequencies

---
+++ Week 2/src/TestWordFrequencies.java
@@ -0,0 +1,6 @@
+public class TestWordFrequencies {
+    public static void main(String[] args) {
+        WordFrequencies wordFrequencies = new WordFrequencies();
+        wordFrequencies.tester();
+    }
+}
